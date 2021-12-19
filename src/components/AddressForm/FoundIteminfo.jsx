39d2f28eb6b5ad7e5eb7da4545009ba6184c5a26
Joymania <joymania_pust@outlook.com>
2021-12-19 21:17:07 +0600

    address component bug fixing

--- src/components/AddressForm/FoundIteminfo.jsx
+++ src/components/AddressForm/FoundIteminfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Card from '../utilities/Card/Card';
 
 const FoundIteminfo = ({savedAddressInfo,getAddressData,activeButtonText}) => {
@@ -8,10 +9,13 @@ const FoundIteminfo = ({savedAddressInfo,getAddressData,activeButtonText}) => {
         {
         (data)?
             <Card className="mb-16 hover-card pointer">
-        <h4 className="t-16 t-bold mb-8">{savedAddressInfo.text}</h4>
-        <p className=" mb-4">{data.Name}-{data.Mobile}-{data.Email}</p>
-        <p>{data.Province },{data.District },{data.Upazila}</p>
-        <p>{data.Remarks}</p>
+              <span >
+              <h4 className="t-16 t-bold mb-8">{savedAddressInfo.text}</h4>
+              <p className=" mb-4">{data.Name}-{data.Mobile}-{data.Email}</p>
+              <p>{data.Province },{data.District },{data.Upazila}</p>
+              <p>{data.Remarks}</p>
+              </span>
+        
       </Card>
         :
         <Card className="mb-16 hover-card pointer">
