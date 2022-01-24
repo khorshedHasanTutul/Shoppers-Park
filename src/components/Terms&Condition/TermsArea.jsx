2129acf88ddfd9af27fbad07931e81e11a734b5b
Joymania <joymania_pust@outlook.com>
2022-01-24 17:47:32 +0600

    funeldesign&dynamic

--- src/components/Terms&Condition/TermsArea.jsx
+++ src/components/Terms&Condition/TermsArea.jsx
@@ -5,7 +5,7 @@ const TermsArea = () => {
         <section class="return-policy-area">
                 <div class="container">
                     <div class="return-policy-main">
-                        <h2>Cancle Order: </h2>
+                        {/* <h2>Cancle Order: </h2>
                         <ul>
                             <li><small>01.</small>If customer want to cancel the order then they hasve to cancel before 1:10 PM. 
                                 After 1:10 PM, cancellation will NOT be accepted.</li>
@@ -13,42 +13,35 @@ const TermsArea = () => {
                             <li><small>03.</small>For medicine orders, you must submit or show your prescriptions while ordering or receiving medicines 
                                 from us. Otherwise, Shopper Perk may cancel the order and return the products.</li>
                            
+                        </ul> */}
+
+                        <h2>Delivery Method:</h2>
+                        <ul>
+                            <li>After completion payment, we will deliver within 3 days.</li>
                         </ul>
 
-                        <h2>Disclaimer:</h2>
+                        <h2>Payment Method:</h2>
                         <ul>
-                            <li><small>01.</small>Shopper Perk is a retail pharmacy that resells medications by maintaining the local government law.</li>
-                            <li><small>02.</small>A patient should carefully read the medicines’ full description, dosage, side effect etc. before consumption.</li>
-                            <li><small>03.</small>You must consult with registered doctors before ordering/consuming any medicine. For any medicine related 
-                                consequences, Shopper Perk and/or its representatives, associates, owners will not be responsible.</li>
-                            <li><small>04.</small>Manufactures and importers are the only entities that are solely responsible for the medicines’ quality, 
-                                packaging and trademarks. Shopper Perk only resell those medicine.</li>
-                            <li><small>05.</small>For any emergency, or in the case that immediate medical support is required, consult with a registered 
-                                doctor and purchase medicine from your nearest pharmacy.</li>
-                            <li><small>06.</small>Your order confirmation will be considered after you receive verbal confirmation over the telephone from 
-                                Shopper Perk hotline.</li>
-                            <li><small>07.</small> To have a successful and uninterrupted order, make sure that your device is virus free. It may affect the 
-                                ordering system.</li>
-                            <li><small>08.</small> For a smooth delivery, Shopper Perk will communicate via SMS, email and telephone calls. We may also send you 
-                                offers or promotions periodically through these channels.</li>
-                            <li><small>09.</small> Our system may receive some information from device/computer such as: IP address, location, time, browser 
-                                type etc. This is due to the default communication protocol, and your information will not be used or kept on 
-                                our database.</li> 
-                            <li><small>10.</small> Orders may be delayed or exceed the lead time due to natural disasters, emergency law enforcement 
-                                restrictions & other national/local crises.</li>
-                            <li><small>11.</small> Shopper Perk has the right to cancel any order based on inventory or any issue.</li>   
-                            <li><small>12.</small> Some product or medicine prices may vary from the price stated online.</li>
-                        </ul>
-
-                        <h2>Customer Must Know:</h2>
-                        <ul>
-                            <li><small>01.</small>Seductive, antibiotic and medicine which has side effects will not be delivered without prescription. In that 
-                                case no “Prescription on delivery” will be allowed.</li>
-                            <li><small>02.</small>Shopper Perk has the rights to cancel order if there is no proper documentation from the customer regarding 
-                                the medicine.</li>
-                            <li><small>03.</small>Without signing of NOC, products will not be delivered</li>
+                            <li><small>01.</small> 100% pre-payment or 50% pre- payment for some selective items through Bkash, Or deposit cash to our account..</li>
+                            <li><small>02.</small>
+                            BKash - (Payment via Mobile).</li>
+                            <li><small>03.</small>Deposit to our Bank Account</li>
+                            <li><small>04.</small>If you have any query or want to place order feel free to call us @ +8801780390222 or message us on our Facebook page.</li>
                            
                         </ul>
+
+                        <h2>Order Process:: </h2>
+                        <h6>For order u can CALL/ INBOX us with the following info:</h6>
+                        <ul>
+                            <li><small>01.</small>Product Name & if possible Product link (inbox)& Qantity </li>
+                            <li><small>02.</small>Customer Name.</li>
+                            <li><small>03.</small>Mobile Number</li>
+                            <li><small>03.</small>Address</li>
+                            <li><small>04.</small>Call/sms on: +8801780390222</li>
+                            <li><small>05.</small>Delivery through courier service in Dhaka.Sunderban</li>
+                            <li><small>06.</small>Courier service & SA-Paribahan for outside Dhaka.</li>
+                        </ul>
+                        <h4>Thank you.</h4>
                     </div>
                 </div>
             </section>
--- src/components/utilities/Alert/PopUpAlert.jsx
+++ src/components/utilities/Alert/PopUpAlert.jsx
@@ -1,13 +1,29 @@
+import { faWeight } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
-const PopUpAlert = ({content,closeModal}) => {
+const PopUpAlert = ({content,closeModal,addingStyle}) => {
     return (
-        <div className="alert-for-all-web">
+        <div className="alert-for-all-web" >
             <div id="demo-modal3" class="modal">
-            <div class="modal__content">
-                <div class="alert-main-area">
-                    <h2>{content}</h2>
-                    <button class="warning" onClick={closeModal}>Ok</button>
+            <div class="modal__content" style={addingStyle && {margin: "0px 0px 200px 0px",width:"60%",height:"350px" }}>
+                <div class="alert-main-area" style={addingStyle &&{display:"flex",alignItems:"center"}}>
+                    {
+                        (!addingStyle) && <h2>{content}</h2>
+                       
+                    }
+                    {
+                        (addingStyle)&&
+                        <>
+                        <p style={{color:"#df2c8a",flex:"0 0 50%"}}>{content}</p> 
+                        <img src="/contents/assets/images/popUp.jpg" style={{width:"100%",height:"350px"}} alt="" />
+                        </>
+                        
+                    }
+                    
+                    {
+                       (!addingStyle) && <button class="warning" onClick={closeModal}>Ok</button>
+                    }
+                    
                 </div>  
                 <a href class="modal__close" onClick={closeModal}>&times;</a>
             </div>
